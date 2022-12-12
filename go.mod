68a4c5065061215a28458a162d7bf5586b55ea6a
shixiao913 <shixiao913@163.com>
2022-12-12 17:21:46 +0800

    transform all encodings to utf8 & use regexp to extract information

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/shixiao913/ants
 
 go 1.19
+
+require (
+	golang.org/x/net v0.4.0
+	golang.org/x/text v0.5.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+)
+
+func fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	r := bufio.NewReader(resp.Body)
+	e := determineCharSet(r)
+	utf8Reader := transform.NewReader(r, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+func determineCharSet(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Println("fetch error:", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+
+var headerRe = regexp.MustCompile(`{"contId"[\s\S]*?,"name":"([\s\S]*?)",`)
 
 func main() {
-	fmt.Println("Hello, Ants!")
+	url := "https://www.thepaper.cn"
+
+	body, err := fetch(url)
+	if err != nil {
+		fmt.Println("fetch error: ", err)
+		return
+	}
+
+	matches := headerRe.FindAllSubmatch(body, -1)
+
+	for _, m := range matches {
+		fmt.Println("fetched news: ", string(m[1]))
+	}
 }
