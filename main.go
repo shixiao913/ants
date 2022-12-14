8904ac80a580cb18f8a8f430b372c501919a934a
shixiao913 <shixiao913@163.com>
2022-12-14 17:08:39 +0800

    xpath, css selector

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"regexp"
 
+	"github.com/PuerkitoBio/goquery"
+	"github.com/antchfx/htmlquery"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
@@ -42,7 +45,69 @@ func determineCharSet(r *bufio.Reader) encoding.Encoding {
 	return e
 }
 
+type Parser interface {
+	Parse(body []byte) ([]string, error)
+}
+
 var headerRe = regexp.MustCompile(`{"contId"[\s\S]*?,"name":"([\s\S]*?)",`)
+
+type ReParser struct {
+}
+
+func (p *ReParser) Parse(body []byte) ([]string, error) {
+	matches := headerRe.FindAllSubmatch(body, -1)
+
+	result := make([]string, 0, len(matches))
+	for _, match := range matches {
+		result = append(result, string(match[1]))
+	}
+
+	return result, nil
+}
+
+type XPathParser struct {
+}
+
+func (p *XPathParser) Parse(body []byte) ([]string, error) {
+	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	nodes := htmlquery.Find(doc, `//div[@class="index_carousel_img__HbOWM"]/a[@target="_blank"]/img`)
+
+	result := make([]string, 0, len(nodes))
+
+	for _, node := range nodes {
+		msg := htmlquery.SelectAttr(node, "alt")
+		result = append(result, msg)
+	}
+
+	return result, nil
+}
+
+/*
+CS stands for CSS Selector
+*/
+type CSParser struct {
+}
+
+func (p *CSParser) Parse(body []byte) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	s := doc.Find("div.index_carousel_img__HbOWM a[target=_blank] img")
+
+	result := make([]string, 0, len(s.Nodes))
+	for _, node := range s.Nodes {
+		msg := htmlquery.SelectAttr(node, "alt")
+		result = append(result, msg)
+	}
+
+	return result, nil
+}
 
 func main() {
 	url := "https://www.thepaper.cn"
@@ -53,9 +118,17 @@ func main() {
 		return
 	}
 
-	matches := headerRe.FindAllSubmatch(body, -1)
+	//parser := &ReParser{}
+	//parser := &XPathParser{}
+	parser := &CSParser{}
 
-	for _, m := range matches {
-		fmt.Println("fetched news: ", string(m[1]))
+	news, err := parser.Parse(body)
+	if err != nil {
+		fmt.Println("parser error: ", err)
+		return
+	}
+
+	for _, msg := range news {
+		fmt.Println("fetched news: ", msg)
 	}
 }
